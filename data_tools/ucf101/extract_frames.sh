a3c4805664b5a02d5c95625d503ef58f35c55614
Lixun Zhang <lixzhang@microsoft.com>
2019-09-10 22:11:44 +0000

    Add path to dense flow for extract frames.

--- data_tools/ucf101/extract_frames.sh
+++ data_tools/ucf101/extract_frames.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/bash env
 
 cd ../
-python build_rawframes.py ../data/ucf101/videos/ ../data/ucf101/rawframes/ --level 2 --flow_type tvl1
+python build_rawframes.py ../data/ucf101/videos/ ../data/ucf101/rawframes/ --level 2 --flow_type tvl1 --df_path ../third_party/dense_flow --num_gpu 4
 echo "Raw frames (RGB and tv-l1) Generated"
 cd ucf101/
